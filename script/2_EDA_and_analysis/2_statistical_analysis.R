ce1b5cf4c4b46eaa8009180648b7952fe0d3a2b6
jliddie-cloud <j.liddie@gwu.edu>
2026-03-09 18:46:06 -0400

    initial stat analysis

--- script/2_EDA_and_analysis/2_statistical_analysis.R
+++ script/2_EDA_and_analysis/2_statistical_analysis.R
@@ -17,13 +17,13 @@ m1a <- glm(n_events ~ inundation_exposure*hurricane_3week +
           data = dat, family = "quasipoisson")
 
 # no separate pre-trend for exposed/control
-m2 <- glm(n_events ~ inundation_exposure*hurricane_3week +
-             as.factor(year) + as.factor(month), 
-           offset = log(total_population),
-           data = dat, family = "quasipoisson")
+  # m2 <- glm(n_events ~ inundation_exposure*hurricane_3week +
+  #              as.factor(year) + as.factor(month), 
+  #            offset = log(total_population),
+  #            data = dat, family = "quasipoisson")
 
 # season fixed effects
-m3 <- glm(n_events ~ inundation_exposure*hurricane_3week +
+m2 <- glm(n_events ~ inundation_exposure*hurricane_3week +
             inundation_exposure*as.factor(year) + inundation_exposure*season, 
           offset = log(total_population),
           data = dat, family = "quasipoisson")
@@ -90,11 +90,89 @@ m1_ITS_season <- glm(n_events ~ hurricane_3week +
 
 all_models <- ls()[grepl("^m[[:digit:]]", ls())]
 
-all_final_coefs <- map_dfr(all_models, ~tidy(eval(as.name(.x))),
+all_models
+
+all_final_coefs <- map_dfr(all_models, 
+                           ~tidy(eval(as.name(.x)), conf.int = TRUE, exponentiate = TRUE),
                            .id = "model_id")
 
-# TODO: make plots of coefficient estimates
 all_final_coefs <- all_final_coefs %>%
   mutate(model_id = as.numeric(model_id),
-         # model_type = case_when(...)
+         model_type = case_when(model_id == 1 ~ "Non-controlled ITS",
+                                model_id == 2 ~ "Non-controlled ITS (season FEs)",
+                                model_id == 3 ~ "CITS: top and bottom flooding quartiles",
+                                model_id == 4 ~ "CITS: main model (3-week)",
+                                model_id == 5 ~ "CITS: main model (5-week)",
+                                model_id == 6 ~ "CITS: main model (8-week)",
+                                model_id == 7 ~ "CITS: season FEs",
+                                model_id == 8 ~ "CITS: mean daily temp + humidity",
+                                model_id == 9 ~ "CITS: max daily temp + humidity",
+                                model_id == 10 ~ "CITS: min daily temp + humidity"),
+         model_group = case_when(model_id %in% c(4, 5, 6) ~ "CITS: main model",
+                                 model_id %in% c(1, 2) ~ "ITS",
+                                 model_id %in% c(7, 8, 9, 10) ~ "Sensitivity analyses")
   )
+
+all_final_fit <- map_dfr(all_models, ~glance(eval(as.name(.x))),
+                         .id = "model_id")
+
+all_final_fit$model_id <- as.numeric(all_final_fit$model_id)
+
+all_final_summary <- left_join(all_final_coefs, all_final_fit)
+
+ggplot(all_final_summary %>% 
+         filter(model_group == "CITS: main model" &
+                grepl("inundation_exposureTRUE:hurricane", term))) +
+  geom_errorbar(aes(x = model_type, y = estimate,
+                    ymin = conf.low, ymax = conf.high, color = model_group),
+                width = 0.5, size = 2, position = position_dodge(width = 0.6)) +
+  geom_point(aes(x = model_type, y = estimate), 
+             color = "black", size = 4, position = position_dodge(width = 0.6)) +
+  # scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  scale_color_manual(values = MetBrewer::met.brewer(name = "Egypt")) +
+  geom_hline(yintercept = 1, color = "darkgrey", linetype = "dashed") +
+  labs(y = "Incidence rate ratio", x = "") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(), 
+        panel.grid.major.x = element_blank(),
+        axis.text.y = element_text(size = 10, color = "black"),
+        axis.text.x = element_text(size = 8, color = "black", face = "bold"),
+        axis.title = element_text(size = 12, color = "black"),
+        # legend.key.height = unit(0.25, "cm"),
+        # legend.key.width = unit(4, "cm"),
+        # legend.text.position = "top",
+        # legend.text = element_text(size = 10),
+        legend.position = "none",
+        plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm"))
+
+if (FALSE) {
+  ggsave("figures/main_results.png", dpi = 600, height = 3, width = 6)
+}
+
+ggplot(all_final_summary %>% 
+         filter(model_group == "Sensitivity analyses" &
+                  grepl("inundation_exposureTRUE:hurricane", term))) +
+  geom_errorbar(aes(x = model_type, y = estimate,
+                    ymin = conf.low, ymax = conf.high, color = model_group),
+                width = 0.5, size = 2, position = position_dodge(width = 0.6)) +
+  geom_point(aes(x = model_type, y = estimate), 
+             color = "black", size = 4, position = position_dodge(width = 0.6)) +
+  # scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  scale_color_manual(values = MetBrewer::met.brewer(name = "Egypt")) +
+  geom_hline(yintercept = 1, color = "darkgrey", linetype = "dashed") +
+  labs(y = "Incidence rate ratio", x = "") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(), 
+        panel.grid.major.x = element_blank(),
+        axis.text.y = element_text(size = 10, color = "black"),
+        axis.text.x = element_text(size = 8, angle = 60, hjust = 1, color = "black", face = "bold"),
+        # legend.key.height = unit(0.25, "cm"),
+        # legend.key.width = unit(4, "cm"),
+        # legend.text.position = "top",
+        # legend.text = element_text(size = 10),
+        legend.position = "none",
+        plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm"))
+
+if (FALSE) {
+  ggsave("figures/initial_sensitivity_results.png", dpi = 600, height = 4, width = 6)
+}
